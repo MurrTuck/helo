57df6c887fffd8bb099cf5116f9c116184bef11a
MurrTuck <murrtuck@gmail.com>
2018-07-15 23:31:32 -0600

    Adding Link package and working on mapping

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,65 +1,91 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 class Dashboard extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            search: '',
-            checkbox: false,
-            posts: []
-        }
-        this.toggleCheckbox = this.toggleCheckbox.bind(this)
-        this.searchInput = this.searchInput.bind(this)
-    }
-    
-    searchInput(e){
-        this.setState({search: e})
-    }
-    
-    resetSearch(){
-        axios.get(`/api/posts/${this.props.id}?search=${this.state.search}&userposts=${this.state.checkbox}`)
-        .then(res => {
-            this.setState({ posts: res.data, search: ''})
-        });
-    }
-    
-    componentDidMount() {
-        this.getPosts()
-    }
+    this.state = {
+      search: "",
+      checkbox: false,
+      posts: []
+    };
+    this.toggleCheckbox = this.toggleCheckbox.bind(this);
+    this.searchInput = this.searchInput.bind(this);
+  }
 
-    getPosts(){
-        axios.get(`/api/posts/${this.props.id}?search=${this.state.search}&userposts=${this.state.checkbox}`)
-        .then(res => {
-            console.log(res)
-            this.setState({posts: res.data})
-        })
-    }
+  searchInput(e) {
+    this.setState({ search: e });
+  }
 
-    toggleCheckbox(){
-        this.setState({checkbox: !this.state.checkbox})
-        this.resetSearch()
-    }
-    
+  resetSearch() {
+    axios
+      .get(
+        `/api/posts/${this.props.id}?search=${this.state.search}&userposts=${
+          this.state.checkbox
+        }`
+      )
+      .then(res => {
+        this.setState({ posts: res.data, search: "" });
+      });
+  }
 
+  componentDidMount() {
+    this.getPosts();
+  }
 
-    render() {
-        let posts = this.state.posts.map((elem, index) => {
-            return <div key={index}>{elem.title}{elem.username}{elem.profilePic}</div>
-        })
-        return <div>
-            This is the Dashboard
-            <input type="text" onChange={(e) => this.searchInput(e.target.value)} placeholder="search" />
-            <button>Search</button>
-            <button onClick={this.resetSearch.bind(this)}>Reset</button>
-            My Posts
-            <input type='checkbox' onClick={() => this.toggleCheckbox()} value={this.state.search}/>
-            {posts}
-          </div>;
-    }
+  getPosts() {
+    axios
+      .get(
+        `/api/posts/${this.props.id}?search=${this.state.search}&userposts=${
+          this.state.checkbox
+        }`
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ posts: res.data });
+      });
+  }
+
+  toggleCheckbox() {
+    this.setState({ checkbox: !this.state.checkbox });
+    this.resetSearch();
+  }
+
+  //Had profile_pic as profilePic and it didn't work. Worked out the bug and now it's working with profile_pic.
+  render() {
+    let posts = this.state.posts.map((elem, index) => {
+      return (
+        <div key={index}>
+          <p>Post Title: {elem.title}</p>
+          <p>Authors Name: {elem.username}</p>
+          <img src={elem.profile_pic} alt="" />
+        </div>
+      );
+    });
+    return (
+      <div>
+        This is the Dashboard
+        <input
+          type="text"
+          onChange={e => this.searchInput(e.target.value)}
+          placeholder="search"
+        />
+        <button>Search</button>
+        <button onClick={this.resetSearch.bind(this)}>Reset</button>
+        My Posts
+        <input
+          type="checkbox"
+          onClick={() => this.toggleCheckbox()}
+          value={this.state.search}
+        />
+        {posts}
+      </div>
+    );
+  }
 }
 
 
